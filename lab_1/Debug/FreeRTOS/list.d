6056b88e2a95c7b7b03b278871e659d20262aeac
Ashlee <ashlee.lee9935@gmail.com>
2025-03-27 16:18:58 +0800

    lab 0, lab 1

---
+++ lab_1/Debug/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/FreeRTOS.h \
+ D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/FreeRTOSConfig.h \
+ D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/projdefs.h \
+ D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/portable.h \
+ D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/deprecated_definitions.h \
+ D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/mpu_wrappers.h \
+ D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/list.h
+D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/FreeRTOS.h:
+D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/FreeRTOSConfig.h:
+D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/projdefs.h:
+D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/portable.h:
+D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/deprecated_definitions.h:
+D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/mpu_wrappers.h:
+D:/LSJ_embeded\ system/workspace/lab_0/FreeRTOS/include/list.h:
